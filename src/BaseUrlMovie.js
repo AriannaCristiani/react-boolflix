d84f09fb88a5979dd98e273fbfe44228b9781049
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-12-12 12:14:16 +0100

    creazione componente con gli url base da interrogare

---
+++ src/BaseUrlMovie.js
@@ -0,0 +1,2 @@
+export const BASE_URI_MOVIE = 'https://api.themoviedb.org/3/search/movie?api_key=1f8067f8ef30a3c06bfa34f1c02587c9'
+export const BASE_URI_TV = 'https://api.themoviedb.org/3/search/tv?api_key=1f8067f8ef30a3c06bfa34f1c02587c9'
