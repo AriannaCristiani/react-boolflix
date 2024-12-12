ab4911e0c86da5a39381787b529377acec7ba3bc
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-12-12 11:59:40 +0100

    creazione struttura base dei componeneti necessari e del global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import GlobalContext from "./Context/GlobalContext"
+import DefaultLayout from "./Layout/DefaultLayout"
 
 function App() {
 
 
   return (
-    <>
-
-    </>
+    //<GlobalContext.Provider value={{}}>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    //</GlobalContext.Provider>
   )
 }
 
---
+++ src/Layout/DefaultLayout.jsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header"
+import Main from "../Components/Main"
+
+
+
+export default function DefaultLayout() {
+    return (
+
+        <div>
+            <Header></Header>
+            <Main></Main>
+        </div>
+
+    )
+}
