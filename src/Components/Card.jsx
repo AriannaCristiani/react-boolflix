867f00fd2c3c847373974e187016dcb1ce3becbb
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-12-12 16:27:53 +0100

    aggiunto oggetto con le immagini da mappare e modifica sulla card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,27 +9,30 @@ const flagMap = {
     fr: '/images/francia.png',
     de: '/images/germania.png',
     es: '/images/spagna.png',
-    jp: '/images/giappone.png',
-    zh: '/images/cina.png',
+    ja: '/images/giappone.png',
+    cn: '/images/cina.png',
     ru: '/images/russia.png',
 };
 
 
 export default function Card({ item, type }) {
+    // immagine di placeholder se non siste un'immagine relativa alla sigla
+    const flagSrc = flagMap[item.original_language] || '/images/placeholder-image.jpg';
+
     return (
         <section className="card">
             {type === 'movie' ? (
                 <>
                     <h3>{item.title}</h3>
                     <p>Titolo originale del film: {item.original_title}</p>
-                    <p>Lingua: {item.original_language}</p>
+                    <p>Lingua: <img src={flagSrc} alt={item.original_language} style={{ width: 30 }} /></p>
                     <p>Voto: {item.vote_average}</p>
                 </>
             ) : (
                 <>
                     <h3>{item.name}</h3>
                     <p>Nome originale della serie: {item.original_name}</p>
-                    <p>Lingua: {item.original_language}</p>
+                    <p>Lingua: <img src={flagSrc} alt={item.original_language} style={{ width: 30 }} /></p>
                     <p>Voto: {item.vote_average}</p>
                 </>
             )}
